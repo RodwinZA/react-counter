8580771e98d08cec4794b1de909ec6b2822479de
rodpiete021 <rodpiete021@student.wethinkcode.co.za>
2023-02-03 22:32:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = React.useState(0)
+
+
+  function add(){
+    setCounter(function(prevCounter){
+      return prevCounter + 1
+    })
+  }
+
+  function subtract(){
+    setCounter(function(prevCounter){
+      return prevCounter - 1
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter">
+        <button className='counter--minus' onClick={subtract}>-</button>
+        <div className='counter--count'>
+          <h1>{counter}</h1>
+        </div>
+        <button className='counter--plus' onClick={add}>+</button>
     </div>
   );
 }
